6a16c39527ee85452a42b4077a711aaeae9c48e5
Jakub Vsetecka <jakub01vsetecka@seznam.cz>
2024-04-17 12:02:46 +0200

    refactor: :construction: Refactor PacketHandler class to process packets and print information

--- src/PacketHandler.cpp
+++ src/PacketHandler.cpp
@@ -7,6 +7,16 @@
  */
 
 #include "PacketHandler.h"
+#include <netinet/ether.h>
+
+#include <iomanip>
+
+#include <arpa/inet.h>
+#include <cstring>
+#include <iomanip> // For std::setw and std::setfill
+#include <iostream>
+#include <netinet/if_ether.h>
+#include <vector>
 
 PacketHandler::PacketHandler(ThreadSafeQueue<PacketData> *queueArg)
     : queue(queueArg), worker(&PacketHandler::run, this) {}
@@ -34,6 +44,44 @@ void PacketHandler::stop() {
     queue->push(stopPacket);
 }
 
-void PacketHandler::processPacket(const PacketData &packet) {
-    std::cout << packet << std::endl;
+void PacketHandler::processPacket(const PacketData &packetData) {
+    std::cout << "Processing packet: " << std::endl;
+    std::cout << packetData << std::endl;
+
+    if (packetData.getPacket().size() < sizeof(struct ether_header)) {
+        std::cerr << "Packet too short to process" << std::endl;
+        return; // Ensure the packet is large enough to contain an Ethernet header
+    }
+
+    // Properly accessing the packet data
+    const struct ether_header *eth_header = reinterpret_cast<const struct ether_header *>(packetData.getData());
+    u_int16_t type = ntohs(eth_header->ether_type); // Network to host short conversion
+
+    // Print source and destination MAC addresses
+    std::cout << "Destination MAC: ";
+    for (int i = 0; i < 6; ++i) {
+        std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(eth_header->ether_dhost[i]);
+        if (i < 5) std::cout << ":";
+    }
+    std::cout << "\nSource MAC: ";
+    for (int i = 0; i < 6; ++i) {
+        std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(eth_header->ether_dhost[i]);
+        if (i < 5) std::cout << ":";
+    }
+    std::cout << std::endl;
+
+    switch (type) {
+    case ETHERTYPE_IP:
+        std::cout << "IPv4 Packet" << std::endl;
+        break;
+    case ETHERTYPE_ARP:
+        std::cout << "ARP Packet" << std::endl;
+        break;
+    case ETHERTYPE_IPV6:
+        std::cout << "IPv6 Packet" << std::endl;
+        break;
+    default:
+        std::cout << "Other Type: 0x" << std::hex << type << std::endl;
+        break;
+    }
 }
--- src/headers/PacketHandler.h
+++ src/headers/PacketHandler.h
@@ -12,18 +12,47 @@
 #include "PacketData.h"
 #include "ThreadSafeQueue.h"
 
+/**
+ * @brief Packet handler
+ */
 class PacketHandler {
   public:
+    /**
+     * @brief Construct a new Packet Handler object
+     *
+     * @param queueArg
+     */
     PacketHandler(ThreadSafeQueue<PacketData> *queueArg);
+
+    /**
+     * @brief Destroy the Packet Handler object
+     */
     ~PacketHandler();
 
+    /**
+     * @brief Run the packet handler
+     *
+     * @return void
+     */
     void run();
+
+    /**
+     * @brief Stop the packet handler
+     *
+     * @return void
+     */
     void stop();
 
   private:
     ThreadSafeQueue<PacketData> *queue;
     std::thread worker;
 
+    /**
+     * @brief Process the packet
+     *
+     * @param packet
+     * @return void
+     */
     void processPacket(const PacketData &packet);
 };
 
